5ad4c11c3cc6283d928be94188479bbce0e350d4
Arthur Yang <yangyemeng@dingfudata.com>
2017-01-18 11:28:15 +0800

    tab to 4 space for all golang file

--- src/raft/persister.go
+++ src/raft/persister.go
@@ -12,50 +12,50 @@ package raft
 import "sync"
 
 type Persister struct {
-	mu        sync.Mutex
-	raftstate []byte
-	snapshot  []byte
+    mu        sync.Mutex
+    raftstate []byte
+    snapshot  []byte
 }
 
 func MakePersister() *Persister {
-	return &Persister{}
+    return &Persister{}
 }
 
 func (ps *Persister) Copy() *Persister {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	np := MakePersister()
-	np.raftstate = ps.raftstate
-	np.snapshot = ps.snapshot
-	return np
+    ps.mu.Lock()
+    defer ps.mu.Unlock()
+    np := MakePersister()
+    np.raftstate = ps.raftstate
+    np.snapshot = ps.snapshot
+    return np
 }
 
 func (ps *Persister) SaveRaftState(data []byte) {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	ps.raftstate = data
+    ps.mu.Lock()
+    defer ps.mu.Unlock()
+    ps.raftstate = data
 }
 
 func (ps *Persister) ReadRaftState() []byte {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return ps.raftstate
+    ps.mu.Lock()
+    defer ps.mu.Unlock()
+    return ps.raftstate
 }
 
 func (ps *Persister) RaftStateSize() int {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return len(ps.raftstate)
+    ps.mu.Lock()
+    defer ps.mu.Unlock()
+    return len(ps.raftstate)
 }
 
 func (ps *Persister) SaveSnapshot(snapshot []byte) {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	ps.snapshot = snapshot
+    ps.mu.Lock()
+    defer ps.mu.Unlock()
+    ps.snapshot = snapshot
 }
 
 func (ps *Persister) ReadSnapshot() []byte {
-	ps.mu.Lock()
-	defer ps.mu.Unlock()
-	return ps.snapshot
+    ps.mu.Lock()
+    defer ps.mu.Unlock()
+    return ps.snapshot
 }
